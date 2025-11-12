b9b788bd47451a4ecb35117e7b684f61347aef48
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-11-12 15:36:07 +0300

    creating index.html,script.js,style.css files
    linking the files
    
    creating and testing add subtract multiply divide
     functions in the script.js file

---
+++ script.js
@@ -0,0 +1,17 @@
+const add=(num1,num2)=>{
+    console.log(num1+num2)
+}
+add(1,2)
+const subtract=(num1,num2)=>{
+    console.log(num1-num2)
+}
+subtract(2,4)
+const multiply=(num1,num2)=>{
+    console.log(num1*num2)
+}
+multiply(2,3)
+
+const divide=(num1,num2)=>{
+    console.log(num1/num2)
+}
+divide(10,5)
