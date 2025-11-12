bfa5d576506ad321c5018fa527cbd370435da741
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-11-12 17:45:38 +0300

    added operate function to the script.js file
    modified add subtract multiply divide functions
        to return results and log to the console

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 const add=(num1,num2)=>{
-    console.log(num1+num2)
+    let result = num1+num2
+    console.log(result)
+    return result
 }
 add(1,2)
 const subtract=(num1,num2)=>{
-    console.log(num1-num2)
+    let result = num1-num2
+    console.log(result)
+    return result
 }
 subtract(2,4)
 const multiply=(num1,num2)=>{
-    console.log(num1*num2)
+    let result = num1*num2
+    console.log(result)
+    return result
 }
 multiply(2,3)
 
 const divide=(num1,num2)=>{
-    console.log(num1/num2)
+    let result = num1/num2
+    console.log(result)
+    return result
 }
 divide(10,5)
 
 const firstNum =''
 const secondNum =''
 const operator =''
+
+const operate =(firstNum,operator,secondNum)=>{
+    if(operator==="+"){
+        return add(firstNum,secondNum)
+    }
+    if(operator==="-"){
+        return subtract(firstNum,secondNum)
+    }
+    if(operator==="*"){
+        return multiply(firstNum,secondNum)
+    }
+    if(operator==="/"){
+        return divide(firstNum,secondNum)
+    }
+
+
+}
+operate(firstNum,operator,secondNum)
