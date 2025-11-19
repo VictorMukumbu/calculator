86ba523436c7dc462ccdc8775592bd7e2d900da9
VictorMukumbu <57131903+VictorMukumbu@users.noreply.github.com>
2025-11-19 15:08:36 +0300

    modified add,subtract,multiply,divide,operate functions
    updated dom consts
    
    updated numberBtn event listener

--- script.js
+++ script.js
@@ -3,48 +3,75 @@ const add=(num1,num2)=>{
     console.log(result)
     return result
 }
-add(1,2)
 const subtract=(num1,num2)=>{
     let result = num1-num2
     console.log(result)
     return result
 }
-subtract(2,4)
 const multiply=(num1,num2)=>{
     let result = num1*num2
     console.log(result)
     return result
 }
-multiply(2,3)
 
 const divide=(num1,num2)=>{
+    if (num2 === 0) {
+        console.log("Error: Division by zero");
+        return "Error";
+    }
     let result = num1/num2
     console.log(result)
     return result
 }
-divide(10,5)
 
-const firstNum =''
-const secondNum =''
-const operator =''
 
 const operate =(firstNum,operator,secondNum)=>{
+    const num1=parseFloat(firstNum)
+    const num2 = parseFloat(secondNum)
+
     if(operator==="+"){
-        return add(firstNum,secondNum)
+        return add(num1,num2)
     }
     if(operator==="-"){
-        return subtract(firstNum,secondNum)
+        return subtract(num1,num2)
     }
     if(operator==="*"){
-        return multiply(firstNum,secondNum)
+        return multiply(num1,num2)
     }
     if(operator==="/"){
-        return divide(firstNum,secondNum)
+        return divide(num1,num2)
     }
 
 
 }
-operate(firstNum,operator,secondNum)
 
-const container =document.getElementById(calculator)
-const displayBox =document.createElement("div")
+const container =document.getElementById("calculator")
+
+const display =document.getElementById("display")
+const numberBtn =document.querySelectorAll(".number")
+const operatorBtn =document.querySelectorAll(".operator")
+const clearBtn =document.querySelector(".clear")
+const equalBtn =document.querySelector(".equal")
+
+let firstNum =""
+let secondNum = ""
+let operator = ""
+let waitingForSecondNum = false
+
+numberBtn.forEach(element => {
+   element.addEventListener("click",()=>{
+    if (waitingForSecondNum) {
+        secondNum += element.textContent
+        display.textContent =secondNum
+        
+    } else {
+        firstNum += element.textContent
+        display.textContent=firstNum
+        
+    }
+        
+    }) 
+});
+
+
+
